b4e3f3e9fb54e9e2a82d1552f7852768ae19df20
khomotso gojela <motsoak49@gmail.com>
2024-02-22 12:18:01 +0200

    init commit

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input('What is your name: ')
+age = input('What is your age: ')
+location = input('Where are you from: ')
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
